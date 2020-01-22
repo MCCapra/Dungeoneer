7b56af861e0192f8295b66524b64641a4e5a2f46
Michael Capra <michael.c.capra@gmail.com>
2020-01-22 15:22:27 -0500

    Initial Commit
    Mup

---
+++ Dungeoneer/Assets/Scripts/Entity.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    /*
+     * Michael Capra
+     * Entity Class: Holds stats for characters  
+     * 1/22/2020
+     */
+
+
+    //Fields 
+    public int hitpoints; //Current hitpoints
+    public int maxHitpoints; //Maximum hitpoints
+    public int attack;
+    public int defense;
+    public int magic;
+    public int magDefense;
+    public int speed;
+    public string e_name;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Calculation for the physical damage dealt by the entity
+    public int CalculatePhysicalDamage()
+    {
+        int dmg = 0;
+
+        //calculation here
+
+        return dmg;
+    }
+
+    public int CalculateMagicDamage()
+    {
+        int dmg = 0;
+
+        //calculation here
+
+        return dmg;
+    }
+}
