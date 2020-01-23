232913b497df352f0c8cf2cd89883f5c23d469af
Michael Capra <michael.c.capra@gmail.com>
2020-01-23 16:48:12 -0500

    Entity Class Created

---
+++ Dungeoneer/Assets/Scripts/Effect.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Effect : MonoBehaviour
+{
+    /*
+     * Michael Capra
+     * Effect Class: Represents any effect in the game (debuff, buff, passive, etc...)
+     * 1/23/2020
+     */
+
+    public string e_name; //name of the effect
+
+}
