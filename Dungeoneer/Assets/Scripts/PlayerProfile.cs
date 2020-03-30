0e7888932511167e9e858836d0136c5354c31359
Michael Capra <michael.c.capra@gmail.com>
2020-03-30 16:53:54 -0400

    Singleton for Player Profile created

---
+++ Dungeoneer/Assets/Scripts/PlayerProfile.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProfile : MonoBehaviour
+{
+    public static PlayerProfile instance = null;
+    public string playerName;
+    public List<GameObject> party; //Characters the player is actively using
+    public List<GameObject> characters; //Characters in the player's posession
+    public int gold = 0;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
