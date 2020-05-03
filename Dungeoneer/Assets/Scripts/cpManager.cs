d1094f0d81402c1628892747a919a191fe8f307f
exa9027 <epadler@gmail.com>
2020-05-02 22:23:27 -0400

    changing parties work

--- Dungeoneer/Assets/Scripts/cpManager.cs
+++ Dungeoneer/Assets/Scripts/cpManager.cs
@@ -6,21 +6,61 @@ using UnityEngine.UI;
 
 public class cpManager : MonoBehaviour
 {
+    public PlayerProfile playerProfile;
+    public GameObject priestButton;
+    public GameObject rogueButton;
+    public GameObject thaumaturgeButton;
+    public GameObject warriorButton;
+    public GameObject demonkinButton;
+    public GameObject paladinButton;
+    public GameObject contentObj;
+
+    public List<GameObject> characterImgs;
+
+    public GameObject changePanel;
+
+    public int changeIndex = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerProfile = GameObject.Find("PlayerProfile").GetComponent<PlayerProfile>();
+        for (int i = 0; i < playerProfile.party.Count; i++)
+        {
+            characterImgs[i].GetComponent<Image>().sprite = playerProfile.party[i].GetComponent<Character>().icon;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void goToFight()
     {
         Debug.Log("click");
         SceneManager.LoadScene("fightingScene");
+    }
+
+    public void openChange()
+    {
+        changePanel.SetActive(true);
+    }
+
+    public void SetIndex0()
+    {
+        changeIndex = 0;
+    }
+    public void SetIndex1()
+    {
+        changeIndex = 1;
+    }
+    public void SetIndex2()
+    {
+        changeIndex = 2;
+    }
+    public void SetIndex3()
+    {
+        changeIndex = 3;
     }
 }
---
+++ Dungeoneer/Assets/characterButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class characterButton : MonoBehaviour
+{
+    public GameObject character;
+    public PlayerProfile playerProfile;
+
+    public List<GameObject> characterImgs;
+    public GameObject changePanel;
+
+    public cpManager manager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerProfile = GameObject.Find("PlayerProfile").GetComponent<PlayerProfile>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetParty()
+    {
+        playerProfile.party[manager.changeIndex] = character;
+        characterImgs[manager.changeIndex].GetComponent<Image>().sprite = character.GetComponent<Character>().icon;
+        changePanel.SetActive(false);
+    }
+}
